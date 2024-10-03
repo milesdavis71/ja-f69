e10ad53cd093fcedb95f81fc3dabec0da4dd59fc
heve.istvan <heve.istvan@gmail.com>
2024-10-03 06:46:14 +0000

    makethumbsf6 első futtatása

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,11 +7,13 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
+    pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
     pkgs.yarn
+    pkgs.exiftool
+    pkgs.imagemagick
   ];
 
   # Sets environment variables in the workspace
