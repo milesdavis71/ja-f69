4691cbb7757393c75984fb04a1e023623b0e19e5
heve.istvan <heve.istvan@gmail.com>
2025-01-20 10:30:22 +0000

    iterate2 felülírva, slider animáció beállítva

--- src/helpers/iterate2.js
+++ src/helpers/iterate2.js
@@ -1,6 +1,6 @@
 module.exports =  function(context, options) {
     let result = '';
-    for (let i = 0; i < 3 && i < context.length; i++) {
+    for (let i = 0; i < 2 && i < context.length; i++) {
       result += options.fn(context[i]);
     }
     return result;
